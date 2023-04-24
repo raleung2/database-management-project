8b6777bf2071e0a879e14ce7730cc55446d44142
raleung2 <70629703+raleung2@users.noreply.github.com>
2023-04-23 22:43:45 -0700

    add p1 to pp1

--- 4-p1-to-pp1.py
+++ 4-p1-to-pp1.py
@@ -1,1 +1,43 @@
+import psycopg2
+from tabulate import tabulate
+
 #Group 3/9 - The product p1 changes its name to pp1 in Product and Stock
+print("Beginning of pk-fk.py")
+
+conn = psycopg2.connect(
+    host = "localhost",
+    port = "8200",
+    database = "cs623progproj",
+    user = "raffertyleung",
+    password = "postgres"
+)
+
+print(conn)
+
+#for isolation: SERIALIZABLE
+conn.set_isolation_level(3)
+
+#for atomicity
+conn.autocommit = False
+
+#add change p1 to pp1
+try:
+  curr = conn.cursor()
+
+  #update product name in product table
+  curr.execute("UPDATE Product SET pname = 'pp1' WHERE prod = 'p1';")
+
+  #update product name in stock table
+  curr.execute("UPDATE Stock SET pname = 'pp1' WHERE prod = 'p1';") #this part does not make sense******
+except (Exception, psycopg2.DatabaseError) as err:
+    print(err)
+    print("Transactions incomplete- database rollback")
+    conn.rollback()
+finally:
+    if conn:
+        conn.commit()
+        print("p1 to pp1 change made")
+        curr.close()
+        conn.close()
+        print("PSQL connection is now closed")
+print("End")
